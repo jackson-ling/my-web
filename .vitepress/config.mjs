c3e1051fd44305f923fcbe3df36011d1ad997b30
jackson-ling <1524205121@qq.com>
2024-12-15 01:31:22 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-   
+   base: '/my-web/', 
   title: "jackson凌's site",
   description: "A VitePress Site",
   themeConfig: {
